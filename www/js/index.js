1d08ffc0961b84f08ac30d3bf52f65bcfa956cfc
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-09-07 15:48:09 +0300

    Added loading screen

--- www/js/index.js
+++ www/js/index.js
@@ -22,91 +22,100 @@ var app = {
   //
   // The scope of 'this' is the event.
   onDeviceReady: function () {
-    moment.locale('fi');
     $.getJSON('https://madweek.metatavu.io/wp-json/wp/v2/posts?per_page=50', function (posts) {
-      var slides = [];
-      for (var i = 0; i < posts.length; i++) {
-        var post = posts[i];
-        switch (post.postType) {
-          case 'frontpage':
-            frontpage = {
-              title: post.title.rendered,
-              content: post.content.rendered,
-            };
-            break;
-          case 'info':
-            infopage = {
-              title: post.title.rendered,
-              content: post.content.rendered,
-            };
-            break;
-          case 'event':
-            var open = JSON.parse(post.open);
-            var openMoment = open.map( function(eventOpen){ 
-              return {
-                date: eventOpen.date,
-                opens: moment(eventOpen.date +' '+eventOpen.opens, 'D.M. H:mm'),
-                closes: moment(eventOpen.date +' '+eventOpen.closes, 'D.M. H:mm') 
-              };
-            });
-            var extraContent = '<p><b>Missä:</b><br/>'+post.blm_formatted_address+'</p><p><b>Milloin:</b><br/>';
-            for(var j = 0; j < openMoment.length;j++) {
-              extraContent+= '<span>'+openMoment[j].opens.format('dd D.M. H:mm')+' - '+openMoment[j].closes.format('H:mm')+'</span><br/>';
-            }
-            eventpages[post.slug] = {
-              slug: post.slug,
-              title: post.title.rendered,
-              content: post.content.rendered + extraContent,
-              latitude: post.blm_latitude,
-              longitude: post.blm_longitude,
-              dates:open.map( function(eventOpen){ return eventOpen.date; }), 
-              open: openMoment
-            }
-            slides.push(post.slug);
-            break;
-        }
-      }
-      slides.sort();
-      for(var i = 0; i < slides.length;i++) {
-        var slide = slides[i];
-        var slideContainer = $('<div></div>');
-        slideContainer.addClass('swiper-slide');
-        slideContainer.append($('<div></div>')
-          .addClass('swiper-container')
-          .addClass('swiper-container-inner')
-          .append(
-            $('<div></div>')
-              .addClass('swiper-wrapper')
-              .append(
-                $('<div></div>')
-                  .addClass('swiper-slide')
-                  .append(
-                    $('<h3></h3>')
-                      .addClass('content-title')
-                      .text(eventpages[slide].title)
-                  )
-                  .append(
-                    $('<div></div>')
-                      .addClass('main-content')
-                      .html(eventpages[slide].content)
-                  )
-              )
-          )
-          .append(
-            $('<div></div>')
-              .addClass('swiper-scrollbar')
-          )
-        );
-        $('.events-wrapper').append(slideContainer);
-      }
-      app.renderFrontPage();
+      app.initApp(posts);
+    }).fail(function(err) {
+      $('.load-error')
+        .empty()
+        .append('<p>Virhe ladattaessa sisältöä, yritetään uudelleen...</p>');
+      setTimeout(function(){
+        app.onDeviceReady();
+      }, 2000);
     });
-    $('.menu-item').click(function(){
+  },
+  initApp: function (posts) {
+    moment.locale('fi');
+    var slides = [];
+    for (var i = 0; i < posts.length; i++) {
+      var post = posts[i];
+      switch (post.postType) {
+        case 'frontpage':
+          frontpage = {
+            title: post.title.rendered,
+            content: post.content.rendered,
+          };
+          break;
+        case 'info':
+          infopage = {
+            title: post.title.rendered,
+            content: post.content.rendered,
+          };
+          break;
+        case 'event':
+          var open = JSON.parse(post.open);
+          var openMoment = open.map(function (eventOpen) {
+            return {
+              date: eventOpen.date,
+              opens: moment(eventOpen.date + ' ' + eventOpen.opens, 'D.M. H:mm'),
+              closes: moment(eventOpen.date + ' ' + eventOpen.closes, 'D.M. H:mm')
+            };
+          });
+          var extraContent = '<p><b>Missä:</b><br/>' + post.blm_formatted_address + '</p><p><b>Milloin:</b><br/>';
+          for (var j = 0; j < openMoment.length; j++) {
+            extraContent += '<span>' + openMoment[j].opens.format('dd D.M. H:mm') + ' - ' + openMoment[j].closes.format('H:mm') + '</span><br/>';
+          }
+          eventpages[post.slug] = {
+            slug: post.slug,
+            title: post.title.rendered,
+            content: post.content.rendered + extraContent,
+            latitude: post.blm_latitude,
+            longitude: post.blm_longitude,
+            dates: open.map(function (eventOpen) { return eventOpen.date; }),
+            open: openMoment
+          }
+          slides.push(post.slug);
+          break;
+      }
+    }
+    slides.sort();
+    for (var i = 0; i < slides.length; i++) {
+      var slide = slides[i];
+      var slideContainer = $('<div></div>');
+      slideContainer.addClass('swiper-slide');
+      slideContainer.append($('<div></div>')
+        .addClass('swiper-container')
+        .addClass('swiper-container-inner')
+        .append(
+        $('<div></div>')
+          .addClass('swiper-wrapper')
+          .append(
+          $('<div></div>')
+            .addClass('swiper-slide')
+            .append(
+            $('<h3></h3>')
+              .addClass('content-title')
+              .text(eventpages[slide].title)
+            )
+            .append(
+            $('<div></div>')
+              .addClass('main-content')
+              .html(eventpages[slide].content)
+            )
+          )
+        )
+        .append(
+        $('<div></div>')
+          .addClass('swiper-scrollbar')
+        )
+      );
+      $('.events-wrapper').append(slideContainer);
+    }
+    $('.menu-item').click(function () {
       var target = $(this).attr('data-target');
       $('.active').removeClass('active');
       $(this).parent().addClass('active');
       $('.navbar-toggle').click();
-      switch(target) {
+      switch (target) {
         case 'frontpage':
           app.renderFrontPage();
           break;
@@ -124,11 +133,11 @@ var app = {
           break;
       }
     });
-    $(document).on('click', '.date-timetable', function(){
+    $(document).on('click', '.date-timetable', function () {
       var date = $(this).attr('data-date');
       app.renderTimeTable(date);
     });
-    $(document).on('click', '.show-event-data', function(){
+    $(document).on('click', '.show-event-data', function () {
       var slug = $(this).attr('data-slug');
       var event = eventpages[slug];
       bootbox.dialog({
@@ -136,6 +145,8 @@ var app = {
         message: event.content
       });
     });
+    $('.load-container').hide();
+    app.renderFrontPage();
   },
   renderPage: function (page) {
     $('.default-container .main-content').html(page.content);
@@ -144,36 +155,36 @@ var app = {
     $('.map-container').hide();
     $('.default-container').show();
   },
-  renderFrontPage: function(){
+  renderFrontPage: function () {
     app.renderPage(frontpage);
   },
-  renderInfoPage: function(){
+  renderInfoPage: function () {
     app.renderPage(infopage);
   },
-  renderMapPage: function(){
+  renderMapPage: function () {
     $('.default-container').hide();
     $('.events-container').hide();
     $('.map-container').show();
-    if(!mapInitialized) {
+    if (!mapInitialized) {
       mapInitialized = true;
       var layer = new L.StamenTileLayer('watercolor');
       map = new L.Map('map');
       map.addLayer(layer);
-      for(var slug in eventpages) {
-        if(eventpages.hasOwnProperty(slug)) {
+      for (var slug in eventpages) {
+        if (eventpages.hasOwnProperty(slug)) {
           var event = eventpages[slug];
           var marker = L.marker({
             lat: event.latitude,
             lng: event.longitude
           }).addTo(map);
-          marker.on('click', function(event){
-            return function(){
+          marker.on('click', function (event) {
+            return function () {
               bootbox.dialog({
                 title: event.title,
                 message: event.content
               });
             };
-          }(event));
+          } (event));
         }
       }
       map.setView({
@@ -181,39 +192,39 @@ var app = {
         lng: 27.272146
       }, 14);
     }
-    navigator.geolocation.getCurrentPosition(function(pos) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
       map.setView({
         lat: pos.coords.latitude,
         lng: pos.coords.longitude
       }, 14);
-    }, function(err) {
+    }, function (err) {
       //If position is not available for some reason, use mikkeli centre which is already initialized
     });
   },
-  renderEventsPage: function(){
+  renderEventsPage: function () {
     $('.default-container').hide();
     $('.map-container').hide();
     $('.events-container').show();
-    if(!eventsInitialized) {
+    if (!eventsInitialized) {
       eventsInitialized = true;
-      new Swiper('.events-container', { });
+      new Swiper('.events-container', {});
       new Swiper('.swiper-container-inner', {
-          scrollbar: '.swiper-scrollbar',
-          direction: 'vertical',
-          slidesPerView: 'auto',
-          mousewheelControl: true,
-          freeMode: true,
-          autoHeight: true
+        scrollbar: '.swiper-scrollbar',
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true,
+        autoHeight: true
       });
     }
   },
   renderTimeTable: function (date) {
     if (typeof date == 'undefined') {
       var dates = [];
-      for(var slug in eventpages) {
-        if(eventpages.hasOwnProperty(slug)) {
+      for (var slug in eventpages) {
+        if (eventpages.hasOwnProperty(slug)) {
           var event = eventpages[slug];
-          for(var i = 0; i < event.dates.length; i++) {
+          for (var i = 0; i < event.dates.length; i++) {
             if (dates.indexOf(event.dates[i]) == -1) {
               dates.push(event.dates[i]);
             }
@@ -234,8 +245,8 @@ var app = {
         }
       });
       var content = '<table class="table">';
-      for(var n = 0; n < moments.length;n++){
-        content += '<tr><td><a data-date="'+moments[n].format('D.M.')+'" class="date-timetable" href="#">'+moments[n].format('dddd D.M')+'</a></td></tr>';
+      for (var n = 0; n < moments.length; n++) {
+        content += '<tr><td><a data-date="' + moments[n].format('D.M.') + '" class="date-timetable" href="#">' + moments[n].format('dddd D.M') + '</a></td></tr>';
       }
       content += '</table>';
       $('.default-container .main-content').html(content);
@@ -245,23 +256,23 @@ var app = {
       $('.default-container').show();
     } else {
       var events = [];
-      for(var slug in eventpages) {
-        if(eventpages.hasOwnProperty(slug)) {
+      for (var slug in eventpages) {
+        if (eventpages.hasOwnProperty(slug)) {
           var event = eventpages[slug];
-          if(event.dates.indexOf(date) !== -1){
+          if (event.dates.indexOf(date) !== -1) {
             events.push(event);
           }
         }
       }
-      for(var i = 0; i < events.length;i++) {
-        for(var j = 0; j < events[i].open.length;j++){
-          if(events[i].open[j].date == date) {
+      for (var i = 0; i < events.length; i++) {
+        for (var j = 0; j < events[i].open.length; j++) {
+          if (events[i].open[j].date == date) {
             events[i].currentOpen = events[i].open[j];
             break;
           }
         }
       }
-      events = events.sort(function(a, b){
+      events = events.sort(function (a, b) {
         if (a.currentOpen.opens.isBefore(b.currentOpen.opens)) {
           return -1;
         } else if (b.currentOpen.opens.isBefore(a.currentOpen.opens)) {
@@ -271,8 +282,8 @@ var app = {
         }
       });
       var content = '<table class="table">';
-      for(var j = 0; j < events.length;j++) {
-        content += '<tr><td><a class="show-event-data" data-slug="'+events[j].slug+'" href="#">'+events[j].currentOpen.opens.format('H:mm')+' - '+events[j].currentOpen.closes.format('H:mm')+' '+events[j].title+'</a></td></tr>';
+      for (var j = 0; j < events.length; j++) {
+        content += '<tr><td><a class="show-event-data" data-slug="' + events[j].slug + '" href="#">' + events[j].currentOpen.opens.format('H:mm') + ' - ' + events[j].currentOpen.closes.format('H:mm') + ' ' + events[j].title + '</a></td></tr>';
       }
       content += '</table>';
       $('.default-container .main-content').html(content);
